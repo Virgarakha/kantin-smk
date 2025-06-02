620ff63cfa25715ea48197b09b59d316b5c0c801
Virgarakha <virgakumara@gmail.com>
2025-06-03 06:23:08 +0800

    Add Route auth in Backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('/signin', [App\Http\Controllers\Auth\AuthController::class, 'signin']);
+        Route::post('/signup', [App\Http\Controllers\Auth\AuthController::class, 'signup']);
+        Route::post('/logout', [App\Http\Controllers\Auth\AuthController::class, 'logout']);
+    });
+})
